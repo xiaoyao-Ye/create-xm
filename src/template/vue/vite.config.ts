4220b203463de229722b34d586accc7f47fc141e
xiaoyao-Ye <yqcyecong@163.com>
2023-02-16 22:07:24 +0800

    feat(template-vue): use auto-import plugins

--- src/template/vue/vite.config.ts
+++ src/template/vue/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      // https://github.com/antfu/unplugin-vue-components#usage
+      dts: './src/components.d.ts'
+    }),
+    AutoImport({
+      // https://github.com/antfu/unplugin-auto-import#configurations
+      imports: [
+        'vue',
+        'vue-router'
+      ],
+      dts: './src/auto-imports.d.ts'
+    }),
+  ],
 })
